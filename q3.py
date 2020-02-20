4c97213543c8f23d035a6be91a5ae01fb1bae34e
Richard Lane <richard.lane@bristol.ac.uk>
2020-02-20 17:01:06 +0000

    some skeletons for some functions

---
+++ q3.py
@@ -0,0 +1,32 @@
+"""
+hello
+"""
+import numpy as np
+
+
+def find_background_mean():
+    """
+    We know the background is 4.8+-0.5; draw a number from this distribution to find the desired background mean to use
+    in each experiment
+
+    """
+    pass
+
+
+def find_poisson_counts(mean):
+    """
+    Draw a number from a Poisson distribution with the given mean
+
+    """
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
